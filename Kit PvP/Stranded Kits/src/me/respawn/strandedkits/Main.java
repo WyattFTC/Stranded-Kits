76c714dff042f383c228233c8ffee2f0f8c38364
Respawnable <wyattquinlan@live.com>
2013-10-27 16:05:46 -0500

    v1.5

--- Kit PvP/Stranded Kits/src/me/respawn/strandedkits/Main.java
+++ Kit PvP/Stranded Kits/src/me/respawn/strandedkits/Main.java
@@ -2,16 +2,20 @@ package me.respawn.strandedkits;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 
-public class Main extends JavaPlugin{
-
+public class Main extends JavaPlugin implements Listener{
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		
 		if(commandLabel.equalsIgnoreCase("kits")) {
@@ -22,17 +26,13 @@ public class Main extends JavaPlugin{
 		
 		if(commandLabel.equalsIgnoreCase("pvp")) {
 			Player player = (Player) sender;
-			player.sendMessage(ChatColor.GOLD + "Stranded Kits > " + ChatColor.WHITE + "you have chosen kit" + ChatColor.GOLD + "PvP");
+			player.sendMessage(ChatColor.GOLD + "Stranded Kits > " + ChatColor.WHITE + "you have chosen kit " + ChatColor.GOLD + "PvP");
 		    for (PotionEffect effect : player.getActivePotionEffects())
 		        player.removePotionEffect(effect.getType());
 			player.getInventory().clear();
 			ItemStack woodsword = new ItemStack(Material.WOOD_SWORD);
 			woodsword.addEnchantment(Enchantment.DAMAGE_ALL, 1);
 			player.getInventory().addItem(woodsword);
-			player.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
-			player.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
-			player.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
-			player.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
 			player.getInventory().addItem(new ItemStack(Material.MUSHROOM_SOUP));
 			player.getInventory().addItem(new ItemStack(Material.MUSHROOM_SOUP));
 			player.getInventory().addItem(new ItemStack(Material.MUSHROOM_SOUP));
